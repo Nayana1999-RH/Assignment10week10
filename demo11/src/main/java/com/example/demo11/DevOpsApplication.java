bab61967ca912555648e2ccc690221ed14145271
Nayanarh <nayanahalesh123@gmail.com>
2022-01-02 19:49:50 +0530

    week10

---
+++ demo11/src/main/java/com/example/demo11/DevOpsApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo11;
+
+
+import org.springframework.boot.CommandLineRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplication.class, args);
+		System.out.println("Hello World");
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ demo11/src/test/java/com/example/demo11/Assignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
